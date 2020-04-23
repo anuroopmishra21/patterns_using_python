d3c52638184a169e33de969440ca717619c6c241
unknown <anuroopmishra21@gmail.com>
2020-04-23 23:44:43 +0530

    Day 2

--- diamond.py
+++ diamond.py
@@ -1,1 +1,20 @@
 # diamond pattern using python
+x= int(input("Enter the no. of rows: "))
+m=x-1
+for i in range(0,x):
+    for k in range(0,m):
+        print(end=" ")
+    for j in range(0,i+1):
+        print("*", end=' ')
+    print("\r")
+    m=m-1
+m=1
+y=x-1
+for i in range(0,x-1):
+    for k in range(0,m):
+        print(end=" ")
+    for j in range(0,y):
+        print("*", end=' ')
+    print("\r")
+    m=m+1
+    y=y-1s
